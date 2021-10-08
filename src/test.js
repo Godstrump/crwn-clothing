bc6e15879bbd77df27cb956be513480a1c6c5284
Godstrump <GGodstrump@gmail.com>
2021-10-08 16:50:53 +0100

    Implemented firebase utils, included ability to store authenticated users into our firestore database

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firestore.firestore();
+
+firestore.collection('users').doc('je8ZW4hxjrx7rAKEeZb6').collection('cartItems').doc('LiLU815sTypN4nj2jy9s');
+firestore.doc('/users/je8ZW4hxjrx7rAKEeZb6/cartItems/LiLU815sTypN4nj2jy9s');
+firestore.collection('/users/je8ZW4hxjrx7rAKEeZb6/cartItems');
